47eed6ef120d2b5a498c8424c8c5792df1d6f8a1
ecarlosdev <ecarlos.dev.code@gmail.com>
2024-06-08 18:12:11 -0600

    feat: add assets

---
+++ packages/assets/lib/gen/assets.gen.dart
@@ -0,0 +1,115 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/arrow-right-rounded.svg
+  SvgGenImage get arrowRightRounded =>
+      const SvgGenImage('assets/icons/arrow-right-rounded.svg');
+
+  /// File path: assets/icons/favorite-filled.svg
+  SvgGenImage get favoriteFilled =>
+      const SvgGenImage('assets/icons/favorite-filled.svg');
+
+  /// File path: assets/icons/favorite.svg
+  SvgGenImage get favorite => const SvgGenImage('assets/icons/favorite.svg');
+
+  /// File path: assets/icons/hamburger-menu.svg
+  SvgGenImage get hamburgerMenu =>
+      const SvgGenImage('assets/icons/hamburger-menu.svg');
+
+  /// File path: assets/icons/search.svg
+  SvgGenImage get search => const SvgGenImage('assets/icons/search.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values =>
+      [arrowRightRounded, favoriteFilled, favorite, hamburgerMenu, search];
+}
+
+class Assets {
+  Assets._();
+
+  static const String package = 'assets';
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
+
+class SvgGenImage {
+  const SvgGenImage(
+    this._assetName, {
+    this.size = null,
+  }) : _isVecFormat = false;
+
+  const SvgGenImage.vec(
+    this._assetName, {
+    this.size = null,
+  }) : _isVecFormat = true;
+
+  final String _assetName;
+
+  static const String package = 'assets';
+
+  final Size? size;
+  final bool _isVecFormat;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme? theme,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture(
+      _isVecFormat
+          ? AssetBytesLoader(_assetName,
+              assetBundle: bundle, packageName: package)
+          : SvgAssetLoader(_assetName,
+              assetBundle: bundle, packageName: package),
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/assets/$_assetName';
+}
