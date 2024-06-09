95e38344825683b165eb0e0b9a305a8f5b0f510b
ecarlosdev <ecarlos.dev.code@gmail.com>
2024-06-09 17:11:06 -0600

    chore: add assets

--- packages/assets/lib/gen/assets.gen.dart
+++ packages/assets/lib/gen/assets.gen.dart
@@ -38,12 +38,204 @@ class $AssetsIconsGen {
       [arrowRightRounded, favoriteFilled, favorite, hamburgerMenu, search];
 }
 
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/pokemon_types
+  $AssetsImagesPokemonTypesGen get pokemonTypes =>
+      const $AssetsImagesPokemonTypesGen();
+}
+
+class $AssetsImagesPokemonTypesGen {
+  const $AssetsImagesPokemonTypesGen();
+
+  /// File path: assets/images/pokemon_types/bug.png
+  AssetGenImage get bug =>
+      const AssetGenImage('assets/images/pokemon_types/bug.png');
+
+  /// File path: assets/images/pokemon_types/dark.png
+  AssetGenImage get dark =>
+      const AssetGenImage('assets/images/pokemon_types/dark.png');
+
+  /// File path: assets/images/pokemon_types/dragon.png
+  AssetGenImage get dragon =>
+      const AssetGenImage('assets/images/pokemon_types/dragon.png');
+
+  /// File path: assets/images/pokemon_types/electric.png
+  AssetGenImage get electric =>
+      const AssetGenImage('assets/images/pokemon_types/electric.png');
+
+  /// File path: assets/images/pokemon_types/fairy.png
+  AssetGenImage get fairy =>
+      const AssetGenImage('assets/images/pokemon_types/fairy.png');
+
+  /// File path: assets/images/pokemon_types/fighting.png
+  AssetGenImage get fighting =>
+      const AssetGenImage('assets/images/pokemon_types/fighting.png');
+
+  /// File path: assets/images/pokemon_types/fire.png
+  AssetGenImage get fire =>
+      const AssetGenImage('assets/images/pokemon_types/fire.png');
+
+  /// File path: assets/images/pokemon_types/flying.png
+  AssetGenImage get flying =>
+      const AssetGenImage('assets/images/pokemon_types/flying.png');
+
+  /// File path: assets/images/pokemon_types/ghost.png
+  AssetGenImage get ghost =>
+      const AssetGenImage('assets/images/pokemon_types/ghost.png');
+
+  /// File path: assets/images/pokemon_types/grass.png
+  AssetGenImage get grass =>
+      const AssetGenImage('assets/images/pokemon_types/grass.png');
+
+  /// File path: assets/images/pokemon_types/ground.png
+  AssetGenImage get ground =>
+      const AssetGenImage('assets/images/pokemon_types/ground.png');
+
+  /// File path: assets/images/pokemon_types/ice.png
+  AssetGenImage get ice =>
+      const AssetGenImage('assets/images/pokemon_types/ice.png');
+
+  /// File path: assets/images/pokemon_types/normal.png
+  AssetGenImage get normal =>
+      const AssetGenImage('assets/images/pokemon_types/normal.png');
+
+  /// File path: assets/images/pokemon_types/poison.png
+  AssetGenImage get poison =>
+      const AssetGenImage('assets/images/pokemon_types/poison.png');
+
+  /// File path: assets/images/pokemon_types/psychic.png
+  AssetGenImage get psychic =>
+      const AssetGenImage('assets/images/pokemon_types/psychic.png');
+
+  /// File path: assets/images/pokemon_types/rock.png
+  AssetGenImage get rock =>
+      const AssetGenImage('assets/images/pokemon_types/rock.png');
+
+  /// File path: assets/images/pokemon_types/steel.png
+  AssetGenImage get steel =>
+      const AssetGenImage('assets/images/pokemon_types/steel.png');
+
+  /// File path: assets/images/pokemon_types/stellar.png
+  AssetGenImage get stellar =>
+      const AssetGenImage('assets/images/pokemon_types/stellar.png');
+
+  /// File path: assets/images/pokemon_types/water.png
+  AssetGenImage get water =>
+      const AssetGenImage('assets/images/pokemon_types/water.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        bug,
+        dark,
+        dragon,
+        electric,
+        fairy,
+        fighting,
+        fire,
+        flying,
+        ghost,
+        grass,
+        ground,
+        ice,
+        normal,
+        poison,
+        psychic,
+        rock,
+        steel,
+        stellar,
+        water
+      ];
+}
+
 class Assets {
   Assets._();
 
   static const String package = 'assets';
 
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size = null});
+
+  final String _assetName;
+
+  static const String package = 'assets';
+
+  final Size? size;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/assets/$_assetName';
 }
 
 class SvgGenImage {
